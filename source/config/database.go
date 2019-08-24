2ff4b900efb9f0e5c6d40f3abc6a81e88741e3c9
fbonhomm <flo-github@outlook.fr>
2019-08-24 20:50:24 +0200

    feat: add config folder and add database file

---
+++ source/config/database.go
@@ -0,0 +1,36 @@
+/**
+ * Created by fbonhomm
+ * Email: flo-github@outlook.fr
+ * Licence: MIT
+ */
+
+package config
+
+import (
+    "fmt"
+    "github.com/fbonhomm/api-go/source/models"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+    "log"
+    "os"
+)
+
+var Db *gorm.DB
+var Err error
+
+func Database() *gorm.DB {
+    Db, Err = gorm.Open(
+        "postgres",
+        fmt.Sprintf(
+            "host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+            os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"),
+            os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD")),
+    )
+
+    if Err != nil {
+        log.Panic("Error connect database", Err)
+    }
+
+    Db.AutoMigrate(&models.User{})
+    return Db
+}
