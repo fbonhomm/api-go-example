b57aa0e78c8fc027e9801d025324c2d2f347d0b8
fbonhomm <flo-github@outlook.fr>
2019-08-24 20:50:52 +0200

    feat: add controller folder with user controller

---
+++ source/controllers/user.go
@@ -0,0 +1,40 @@
+/**
+ * Created by fbonhomm
+ * Email: flo-github@outlook.fr
+ * Licence: MIT
+ */
+
+package controllers
+
+import (
+    "github.com/fbonhomm/api-go/source/config"
+    "github.com/fbonhomm/api-go/source/models"
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+// UserCreate
+func UserCreate(c *gin.Context) {
+    user := models.User{
+        Name: c.PostForm("name"),
+        Email: c.PostForm("email"),
+        Password: c.PostForm("password"),
+    }
+
+    if err := config.Db.Create(&user).Error; err != nil {
+        c.JSON(http.StatusConflict, gin.H{ "error": err.Error() })
+    } else {
+        c.JSON(http.StatusCreated, gin.H{ "item": user })
+    }
+}
+
+// UserGetId
+func UserGetId(c *gin.Context) {
+    user := models.User{}
+
+    if err := config.Db.First(&user, c.Param("id")).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{ "error": err.Error() })
+    } else {
+        c.JSON(http.StatusOK, gin.H{ "item": user })
+    }
+}
