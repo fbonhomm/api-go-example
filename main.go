66ed6dd35d6cc5a0737ac0c70a0674c3b5aa59ff
fbonhomm <flo-github@outlook.fr>
2019-08-24 20:49:10 +0200

    feat add main with user route

---
+++ main.go
@@ -0,0 +1,38 @@
+/**
+ * Created by fbonhomm
+ * Email: flo-github@outlook.fr
+ * Licence: MIT
+ */
+
+package main
+
+import (
+    "github.com/fbonhomm/api-go/source/config"
+    "github.com/fbonhomm/api-go/source/routers"
+    "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
+    "log"
+)
+
+// init
+func init() {
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("No .env file found")
+    }
+}
+
+// main
+func main() {
+    // Initialize Database
+    db := config.Database()
+
+    routerEngine := gin.Default()
+
+    routers.User(routerEngine)
+    // for route := range routers {
+    //     route(routerEngine)
+    // }
+
+    routerEngine.Run()
+    db.Close()
+}
