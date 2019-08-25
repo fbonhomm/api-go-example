24bf072612d10bc477878f7b825e0e292da0a410
fbonhomm <flo-github@outlook.fr>
2019-08-25 17:14:25 +0200

    feat: add user delete route

--- source/routers/user.go
+++ source/routers/user.go
@@ -15,11 +15,8 @@ import (
 // main
 func User(router *gin.Engine) {
     route := router.Group("/users")
-    {
-        route.POST("/", validators.ValidateUserCreate, controllers.UserCreate)
-        route.GET("/:id", validators.ValidateUserGetId, controllers.UserGetId)
-        // route.GET("/", fetchAllTodo)
-        // route.PUT("/:id", updateTodo)
-        // route.DELETE("/:id", deleteTodo)
-    }
+
+    route.POST("/", validators.ValidateUserCreate, controllers.UserCreate)
+    route.GET("/:id", validators.ValidateUserGetId, controllers.UserGetId)
+    route.DELETE("/:id", validators.ValidateUserDeleteId, controllers.UserDeleteId)
 }
