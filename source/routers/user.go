700a8e2e05533c1753f64194afd4edd12418ca9e
fbonhomm <flo-github@outlook.fr>
2019-08-26 20:00:56 +0200

    feat: add middleware auth jwt in routes

--- source/routers/user.go
+++ source/routers/user.go
@@ -8,6 +8,7 @@ package routers
 
 import (
     "github.com/fbonhomm/api-go/source/controllers"
+    "github.com/fbonhomm/api-go/source/middlewares"
     "github.com/fbonhomm/api-go/source/validators"
     "github.com/gin-gonic/gin"
 )
@@ -16,7 +17,7 @@ import (
 func User(router *gin.Engine) {
     route := router.Group("/users")
 
-    route.POST("/", validators.ValidateUserCreate, controllers.UserCreate)
-    route.GET("/:id", validators.ValidateUserGetId, controllers.UserGetId)
-    route.DELETE("/:id", validators.ValidateUserDeleteId, controllers.UserDeleteId)
+    route.POST("/", middlewares.Auth, validators.ValidateUserCreate, controllers.UserCreate)
+    route.GET("/:id", middlewares.Auth, validators.ValidateUserGetId, controllers.UserGetId)
+    route.DELETE("/:id", middlewares.Auth, validators.ValidateUserDeleteId, controllers.UserDeleteId)
 }
